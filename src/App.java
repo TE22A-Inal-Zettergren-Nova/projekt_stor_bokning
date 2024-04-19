e7a3a9361ba5db30f51f2272258973c069a24112
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2024-04-19 18:54:56 +0200

    skapat meny med try catch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+
+    static void meny(){
+        Scanner tb = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Meny \n 1. Boka \n 2. Avboka \n 3. Beräkna vinst \n 4. Avsluta");
+            try {
+                int startval = Integer.parseInt(tb.nextLine());
+
+                switch (startval) {
+                    case 1:
+                        System.out.println("Vill du boka en fönsterplats eller en gångplats?");
+                        running = false;
+                        break;
+                    case 2:
+                        System.out.println("Ditt personnummer krävs för att avboka \n Skriv in ditt personnummer här: ");
+                        running = false;
+                        break;
+                    case 3:
+                        System.out.println("Den totala vinsten är...");
+                        running = false;
+                        break;
+                    case 4:
+                        System.out.println("Programmet avslutas...");
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Detta val finns inte");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Felaktig inmatning. Ange en siffra.");
+            
+        }
     }
 }
+    
+
+
+    public static void main(String[] args) throws Exception {
+            meny();
+            }
+    }
+
